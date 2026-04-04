e52d30db779c8ea9717aac3ba556babe999dc1e3
joey <116466600+joeyzhang-dev@users.noreply.github.com>
2026-04-04 04:56:23 -0400

    add StarButton component, redesign contact page, add cn utility

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "star-btn": "star-btn calc(var(--duration)*1s) linear infinite",
+      },
+      keyframes: {
+        "star-btn": {
+          "0%": { offsetDistance: "0%" },
+          "100%": { offsetDistance: "100%" },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
